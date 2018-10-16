8e04e5900198b111f018e8dc2100715ec05c0674
lianghongh <lianghongh@126.com>
2018-10-16 16:37:48 +0800

    Change some files

--- main.py
+++ main.py
@@ -1,26 +1,52 @@
 from power import power_m
 from nn import Network
 import numpy as np
+import tensorflow as tf
+import os
 
+def benchmark(program_path,act_func,path):
+    if not os.path.exists(path):
+        os.mkdir(path)
 
-if __name__=='__main__':
-    prefix='/home/lianghong/data/result/blackscholes/data'
-    power=np.array([power_m.get_power_list(prefix+'/power',0.1)[0]],dtype=np.float32)
-    events=[]
+    power = np.array([power_m.get_power_list(program_path + '/power', 0.1)[0]], dtype=np.float32) / 10 * 2
+    events = []
     for i in range(8):
-        events.append(power_m.get_event_list(prefix+'/cpu_'+str(i),0.1))
-    length=len(events[0][0])
-    avg_event=np.array([[0 for i in range(length)] for j in range(11)],dtype=np.float32)
+        events.append(power_m.get_event_list(program_path + '/cpu_' + str(i), 0.1))
+    length = len(events[0][0])
+    avg_event = np.array([[0 for i in range(length)] for j in range(11)], dtype=np.float32)
 
     for i in range(8):
         for j in range(11):
-            avg_event[j]=avg_event[j]+np.array(events[i][j])
+            avg_event[j] = avg_event[j] + np.array(events[i][j])
 
     for i in range(11):
-        avg_event[i]=avg_event[i]/8
+        avg_event[i] = avg_event[i] / 8
 
-    model=Network.linear_model_train(avg_event[:8].T,power.T)
-    Network.graph(model(avg_event[:8].T),power.T)
+    # 测试线性模型
+    model = Network.linear_model_train(avg_event[:8].T, power.T)
+    Network.graph(model(avg_event[:8].T), power.T, program_name=program, save_path=path+"/linear")
+
+    # 测试单隐含层模型
+    file_name=path+"/onelayer/prediction_acc"
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    for node in range(4,14):
+        model=Network.onelayer_model(avg_event[:8].T,power.T,layer_node=node,act_func=act_func,path=path,program_name=program)
+        Network.graph(model(avg_event[:8].T), power.T, program_name=program, save_path=path+"/onelayer/"+program+"_"+str(node))
+
+    # 测试双隐含层模型
+    file_name=path+"/twolayer/prediction_acc"
+    if os.path.exists(file_name):
+        os.remove(file_name)
+    for node1 in range(4,14):
+        for node2 in range(4,14):
+            model=Network.twolayer_model(avg_event[:8].T,power.T,layer1_node=node1,layer2_node=node2,act_func=act_func,path=path,program_name=program)
+            Network.graph(model(avg_event[:8].T), power.T, program_name=program, save_path=path+"/twolayer/"+program+"_"+str(node1)+"_"+str(node2))
+
+if __name__=='__main__':
+    program="blackscholes"
+    path = "/home/lianghong/data/result/" + program + "/data"
+    benchmark(path,act_func=tf.nn.relu,path="/home/lianghong/pic")
 
     # l=cov.get_spearman_list("/home/lianghong/data/events_data","/home/lianghong/data/power_data",0.1,300)
     # for i in l:
--- nn/Network.py
+++ nn/Network.py
@@ -1,111 +1,156 @@
 import tensorflow as tf
 import numpy as np
-from tensorflow.examples.tutorials.mnist import input_data
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
+import sys,os
 
-def inference(input_tensor,avg_class,weight1,biases1,weight2,biases2):
-    if avg_class==None:
-        layer1=tf.nn.relu(tf.matmul(input_tensor,weight1)+biases1)
-        return tf.matmul(layer1,weight2)+biases2
-    else:
-        layer1=tf.nn.relu(tf.matmul(input_tensor,avg_class.average(weight1))+avg_class.average(biases1))
-        return tf.matmul(layer1,avg_class.average(weight2))+avg_class.average(biases2)
+def sigmoid(x):
+    return 1/(1+np.e**-x)
 
-def train():
-    mnist = input_data.read_data_sets("MNIST/", one_hot=True)
-    INPUT_NODE, OUTPUT_NODE = 784, 10
-    LAYER1_NODE = 500
-    BATCH_SIZE = 100
-    LEARNING_RATE, LEARNING_RATE_DECAY = 0.8, 0.99
-    RE_RATE = 0.0001
-    TRAINING_STEP = 30000
-    MOVING_AVERAGE_DECAY = 0.99
-    x=tf.placeholder(tf.float32,[None,INPUT_NODE],name='x')
-    y_=tf.placeholder(tf.float32,[None,OUTPUT_NODE],name='y_')
+def relu(x):
+    return np.maximum(0,x)
 
-    weights1=tf.Variable(tf.truncated_normal([INPUT_NODE,LAYER1_NODE],stddev=0.1))
-    biases1=tf.Variable(tf.constant(0.1,shape=[LAYER1_NODE]))
-    weights2=tf.Variable(tf.truncated_normal([LAYER1_NODE,OUTPUT_NODE],stddev=0.1))
-    biases2=tf.Variable(tf.constant(0.1,shape=[OUTPUT_NODE]))
+def tanh(x):
+    a=np.e**x
+    b=np.e**-x
+    return (a-b)/(a+b)
 
-    y=inference(x,None,weights1,biases1,weights2,biases2)
-    global_step=tf.Variable(0,trainable=False)
+def onelayer_model(events,power,layer_node,act_func,path,program_name):
+    scalar = MinMaxScaler()
+    power_trans = power
+    events_trans = scalar.fit_transform(events)
+    activate_func = act_func
+    data_size = len(events_trans)
+    INPUT_FEATURE, LAYER_NODE, OUTPUT_FEATURE = 8, layer_node, 1
+    x = tf.placeholder(tf.float32, [None, INPUT_FEATURE], name='x')
+    y_ = tf.placeholder(tf.float32, [None, OUTPUT_FEATURE], name='y_')
 
-    variable_averages=tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY,global_step)
-    variable_averages_op=variable_averages.apply(tf.trainable_variables())
+    weight1 = tf.Variable(tf.random_normal([INPUT_FEATURE, LAYER_NODE], stddev=0.1))
+    bias1 = tf.Variable(tf.constant(0.0, shape=[LAYER_NODE]))
+    weight2 = tf.Variable(tf.random_normal([LAYER_NODE,OUTPUT_FEATURE], stddev=0.1))
+    bias2 = tf.Variable(tf.constant(0.0, shape=[OUTPUT_FEATURE]))
 
-    average_y=inference(x,variable_averages,weights1,biases1,weights2,biases2)
-    cross_entropy=tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y,labels=tf.argmax(y_,1))
-    cross_entropy_mean=tf.reduce_mean(cross_entropy)
-    re=tf.contrib.layers.l2_regularizer(RE_RATE)
-    regularization=re(weights1)+re(weights2)
-    loss=cross_entropy_mean+regularization
-    learning_rate=tf.train.exponential_decay(LEARNING_RATE,global_step,int(mnist.train.num_examples/BATCH_SIZE),LEARNING_RATE_DECAY)
-
-    train_step=tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,global_step=global_step)
-    with tf.control_dependencies([train_step,variable_averages_op]):
-        train_op=tf.no_op(name='train')
-    correct_prediction=tf.equal(tf.argmax(average_y,1),tf.argmax(y_,1))
-    acc=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
-
-    with tf.Session() as sess:
-        tf.initialize_all_variables().run()
-        validate_feed={x:mnist.validation.images,y_:mnist.validation.labels}
-        test_feed={x:mnist.test.images,y_:mnist.test.labels}
-
-        for i in range(TRAINING_STEP):
-            if i%1000==0:
-                validate_acc=sess.run(acc,feed_dict=validate_feed)
-                print("After %d training steps,validation accuracy using average model is %g " % (i,validate_acc))
-            xs,ys=mnist.train.next_batch(BATCH_SIZE)
-            sess.run(train_op,feed_dict={x:xs,y_:ys})
-
-        test_acc=sess.run(acc,feed_dict=test_feed)
-        print("After %d training steps, test accuracy using average model is %g" % (TRAINING_STEP,test_acc))
-
-
-def onelayer_model(events,power):
-    scalar=MinMaxScaler()
-    power_trans=power
-    events_trans=scalar.fit_transform(events)
-    data_size=len(events_trans)
-    INPUT_FEATURE,LAYER1_NODE,OUTPUT_FEATURE=8,24,1
-    x=tf.placeholder(tf.float32,[None,INPUT_FEATURE],name='x')
-    y_=tf.placeholder(tf.float32,[None,OUTPUT_FEATURE],name='y_')
-    weight1=tf.Variable(tf.random_normal([INPUT_FEATURE,LAYER1_NODE],stddev=0.1))
-    bias1=tf.Variable(tf.constant(0.0,shape=[LAYER1_NODE]))
-    weight2=tf.Variable(tf.random_normal([LAYER1_NODE,OUTPUT_FEATURE],stddev=0.1))
-    bias2=tf.Variable(tf.constant(0.0,shape=[OUTPUT_FEATURE]))
-
-    layer1=tf.nn.sigmoid(tf.matmul(x,weight1)+bias1)
-    y=tf.matmul(layer1,weight2)+bias2
-    global_step=tf.Variable(0)
-    loss=tf.reduce_mean(tf.square(y_-y))
-    decay_rate=tf.train.exponential_decay(0.2,global_step,100,0.98,staircase=True)
-    train_op=tf.train.AdamOptimizer(decay_rate).minimize(loss,global_step=global_step)
-    batch_size=40
+    layer = activate_func(tf.matmul(x, weight1) + bias1)
+    y = tf.matmul(layer, weight2) + bias2
+    global_step = tf.Variable(0)
+    loss = tf.reduce_mean(tf.square(y_ - y))
+    decay_rate = tf.train.exponential_decay(0.1, global_step, 100, 0.98, staircase=True)
+    train_op = tf.train.GradientDescentOptimizer(decay_rate).minimize(loss, global_step=global_step)
+    batch_size = 40
+    print("******Begin Trainning "+program_name+" node:"+str(layer_node)+"******")
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        train_steps=data_size
+        train_steps = 1000
         for i in range(train_steps):
-            start=i*batch_size%data_size
-            end=min(start+batch_size,data_size)
-            sess.run(train_op, feed_dict={x: events_trans[start:end], y_: power_trans[start:end]})
-            if i%100==0:
-                loss_d=sess.run(loss,feed_dict={x:events_trans,y_:power_trans})
+            start = i * batch_size % data_size
+            end = min(start + batch_size, data_size)
+            __, w1, b1, w2, b2= sess.run([train_op, weight1, bias1, weight2, bias2],
+                                                  feed_dict={x: events_trans[start:end], y_: power_trans[start:end]})
+            if i % 100 == 0:
+                loss_d = sess.run(loss, feed_dict={x: events_trans, y_: power_trans})
                 print("After %d training steps,loss on all data is %g" % (i, loss_d))
-        l1=tf.nn.sigmoid(tf.matmul(events_trans,weight1)+bias1)
-        p=sess.run(tf.matmul(l1,weight2)+bias2)
-        avg_loss=np.sum(np.abs(p-power_trans))/np.sum(power_trans)
-        print("Data set size :%d\nAverage loss is %.4f" %(data_size,avg_loss))
-        x=np.arange(data_size)
-        plt.plot(x,p,color='green',label='pred')
-        plt.plot(x,power_trans,color='red',label='real')
-        plt.xlabel("count")
-        plt.ylabel("power /w")
-        plt.legend()
-        plt.show()
+
+        l1 = activate_func(tf.matmul(events_trans, weight1) + bias1)
+        prediction = sess.run(tf.matmul(l1, weight2) + bias2)
+        avg_loss = np.mean(np.abs(prediction - power_trans) / power_trans)
+        print("Avg loss is %f" % (avg_loss))
+        print("******End Trainning******\n")
+        file_name=path+"/onelayer/prediction_acc"
+        if not os.path.exists(path+"/onelayer"):
+            os.mkdir(path+"/onelayer")
+        with open(file_name,"a+",encoding="utf-8") as f:
+            dd="onelayer_model "+"node:"+str(layer_node)+" "+str(1-avg_loss)+"\n"
+            f.write(dd)
+
+        def model(events):
+
+            xx = scalar.transform(events)
+            if act_func==tf.nn.sigmoid:
+                l1 = sigmoid(np.matmul(xx, w1) + b1)
+            elif act_func==tf.nn.relu:
+                l1=relu(np.matmul(xx,w1)+b1)
+            elif act_func==tf.nn.tanh:
+                l1=tanh(np.matmul(xx,w1)+b1)
+            else:
+                print("Activate Function Error!")
+                sys.exit(1)
+
+            l2 = np.matmul(l1, w2) + b2
+            return l2
+
+        return model
+
+def twolayer_model(events,power,layer1_node,layer2_node,act_func,path,program_name):
+    scalar = MinMaxScaler()
+    power_trans = power
+    events_trans = scalar.fit_transform(events)
+    activate_func=act_func
+    data_size = len(events_trans)
+    INPUT_FEATURE, LAYER1_NODE, LAYER2_NODE,OUTPUT_FEATURE = 8, layer1_node,layer2_node, 1
+    x = tf.placeholder(tf.float32, [None, INPUT_FEATURE], name='x')
+    y_ = tf.placeholder(tf.float32, [None, OUTPUT_FEATURE], name='y_')
+
+    weight1 = tf.Variable(tf.random_normal([INPUT_FEATURE, LAYER1_NODE], stddev=0.1))
+    bias1 = tf.Variable(tf.constant(0.0, shape=[LAYER1_NODE]))
+    weight2=tf.Variable(tf.random_normal([LAYER1_NODE,LAYER2_NODE],stddev=0.1))
+    bias2=tf.Variable(tf.constant(0.0,shape=[LAYER2_NODE]))
+    weight3 = tf.Variable(tf.random_normal([LAYER2_NODE, OUTPUT_FEATURE], stddev=0.1))
+    bias3 = tf.Variable(tf.constant(0.0, shape=[OUTPUT_FEATURE]))
+
+    layer1 = activate_func(tf.matmul(x, weight1) + bias1)
+    layer2= activate_func(tf.matmul(layer1,weight2)+bias2)
+    y = tf.matmul(layer2, weight3) + bias3
+    global_step = tf.Variable(0)
+    loss = tf.reduce_mean(tf.square(y_ - y))
+    decay_rate = tf.train.exponential_decay(0.1, global_step, 100, 0.98, staircase=True)
+    train_op = tf.train.GradientDescentOptimizer(decay_rate).minimize(loss, global_step=global_step)
+    batch_size = 40
+    print("******Begin Trainning " + program_name + " node1:" + str(layer1_node) + " node2:"+str(layer2_node)+"******")
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        train_steps = 1000
+        for i in range(train_steps):
+            start = i * batch_size % data_size
+            end = min(start + batch_size, data_size)
+            __,w1,b1,w2,b2,w3,b3=sess.run([train_op,weight1,bias1,weight2,bias2,weight3,bias3], feed_dict={x: events_trans[start:end], y_: power_trans[start:end]})
+            if i % 100 == 0:
+                loss_d = sess.run(loss, feed_dict={x: events_trans, y_: power_trans})
+                print("After %d training steps,loss on all data is %g" % (i, loss_d))
+
+        l1 = activate_func(tf.matmul(events_trans, weight1) + bias1)
+        l2=activate_func(tf.matmul(l1,weight2)+bias2)
+        prediction = sess.run(tf.matmul(l2, weight3) + bias3)
+        avg_loss = np.mean(np.abs(prediction - power_trans) / power_trans)
+        print("Avg loss is %f" % (avg_loss))
+        print("******End Trainning******\n")
+        file_name=path+"/twolayer/prediction_acc"
+        if not os.path.exists(path+"/twolayer"):
+            os.mkdir(path+"/twolayer")
+        with open(file_name,"a+",encoding="utf-8") as f:
+            dd="twolayer_model"+" node1:"+str(layer1_node)+" node2:"+str(layer2_node)+" "+str(1-avg_loss)+"\n"
+            f.write(dd)
+
+        def model(events):
+
+            xx=scalar.transform(events)
+            if act_func==tf.nn.relu:
+                l1=relu(np.matmul(xx,w1)+b1)
+                l2=relu(np.matmul(l1,w2)+b2)
+            elif act_func==tf.nn.sigmoid:
+                l1=sigmoid(np.matmul(xx,w1)+b1)
+                l2=sigmoid(np.matmul(l1,w2)+b2)
+            elif act_func==tf.nn.tanh:
+                l1=tanh(np.matmul(xx,w1)+b1)
+                l2=tanh(np.matmul(l1,w2)+b2)
+            else:
+                print("Activate Function Error!")
+                sys.exit(1)
+
+            l3=np.matmul(l2,w3)+b3
+            return l3
+
+        return model
+
 
 
 def linear_model_train(events,power):
@@ -143,12 +188,15 @@ def linear_model_train(events,power):
             return np.matmul(scalar.transform(events),w)+b
         return model
 
-def graph(prediction,real):
+def graph(prediction,real,program_name,save_path):
     size=len(real)
     x=np.arange(size)
+    plt.figure()
     plt.plot(x,prediction,color='g',label='Prediction')
     plt.plot(x,real,color='r',label='Real')
+    plt.title(program_name+" Power Prediction")
     plt.xlabel('Count')
-    plt.ylabel('Power/W')
+    plt.ylabel('Average Power (W)')
     plt.legend()
-    plt.show()
+    plt.savefig(save_path)
+    plt.close()
